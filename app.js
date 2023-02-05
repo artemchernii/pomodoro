dd11181758fd9d4932265b691d356a90e66df4c8
Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
2023-02-05 15:18:54 +0000

    feat: add html structure and css styles

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 window.onload = function () {
 	console.log('Loaded');
 };
+
+class App {
+	constructor() {
+		console.log('Loaded');
+		this.helloButton = document.querySelector('.hello-button');
+	}
+
+	clickHandler(event) {
+		console.log('Clicked', event);
+	}
+}
+const app = new App();
+
+// Navigator
+console.log('==== Navigator ====');
+console.log(navigator.cookieEnabled);
+console.log(navigator.appName);
+console.log(navigator.language);
+// Location
+console.log('==== Location ====');
+console.log(window.location.href);
+console.log(window.location.hostname);
+console.log(window.location.pathname);
+console.log(window.location.protocol);
+// console.log(window.location.assign('http://google.com'));
+// window.location.assign('http://google.com');
